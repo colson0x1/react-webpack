be8d74f645dfa76a68021078805a18baf464fa4a
Colson <colsoncalifornication@gmail.com>
2024-06-18 12:29:25 +0545

    configure Webpack: add Babel loader in the pipeline

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,15 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/,
+      },
+    ],
+  },
 };
