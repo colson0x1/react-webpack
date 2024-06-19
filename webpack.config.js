3803ca39f65d262759eddba9d626566edbfb3beb
Colson <colsoncalifornication@gmail.com>
2024-06-19 12:12:07 +0545

    vendor asset caching: refactor for vendor splitting - reduce the size of the output bundle.js file: use code splitting to separate application code and third party module code or the vendor code like React, Lodash, Redux, Angular - i.e separate code in two buckets: bundle.js and vendor.js since all major browser vendors are automatically using asset caching: i.e use code splitting along with the browser caching system to decrease the frequency at which the user needs to redownload vendor.js file again and again and again

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,31 @@
 var webpack = require('webpack');
 var path = require('path');
 
+const VENDOR_LIBS = [
+  'react',
+  'lodash',
+  'redux',
+  'react-redux',
+  'react-dom',
+  'faker',
+  'react-input-range',
+  'redux-form',
+  'redux-thunk',
+];
+
 module.exports = {
-  entry: './src/index.js',
+  // entry: './src/index.js',
+  entry: {
+    // Bundle that has all the Application specific code
+    bundle: './src/index.js',
+    // Bundle that has all the module dependencies
+    vendor: VENDOR_LIBS,
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
+    // filename: 'bundle.js',
+    // Dynamically update the output budnle name
+    filename: '[name].js',
   },
   module: {
     rules: [
