3f2f75f1cec914c293c37f5b97e4d469fec79187
Colson <colsoncalifornication@gmail.com>
2024-06-21 13:03:34 +0545

    react js focused optimization @ code splitting with react router plain routes: use plain routes approach instead of jsx based approach to use code splitting effectively with react router i.e transform general JSX structure into a plain JavaScript object which is what React Router does exactly behind the scenes with all the JSX written by the Engineers + use getComponent fn to load the component asynchronously and call the callback after fetching/asynchronously loading child component with System import

--- src/router.js
+++ src/router.js
@@ -3,21 +3,65 @@ import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 import Home from './components/Home';
 import ArtistMain from './components/artists/ArtistMain';
-import ArtistDetail from './components/artists/ArtistDetail';
-import ArtistCreate from './components/artists/ArtistCreate';
-import ArtistEdit from './components/artists/ArtistEdit';
 
+// These modules are being dynamically loaded
+// import ArtistDetail from './components/artists/ArtistDetail';
+// import ArtistCreate from './components/artists/ArtistCreate';
+// import ArtistEdit from './components/artists/ArtistEdit';
+
+// @ Route configuration object
+// Transform JSX route structure into Plain Javascript Object
+const componentRoutes = {
+  component: Home,
+  path: '/',
+  indexRoute: { component: ArtistMain },
+  childRoutes: [
+    {
+      path: 'artists/new',
+      // Asynchronously load the component
+      getComponent(location, cb) {
+        System.import('./components/artists/ArtistCreate').then((module) =>
+          cb(null, module.default),
+        );
+      },
+    },
+    {
+      path: 'artists/:id',
+      getComponent(location, cb) {
+        System.import('./components/artists/ArtistDetail').then((module) =>
+          cb(null, module.default),
+        );
+      },
+    },
+    {
+      path: 'artists/:id/edit',
+      getComponent(location, cb) {
+        System.import('./components/artists/ArtistEdit').then((module) =>
+          cb(null, module.default),
+        );
+      },
+    },
+  ],
+};
+
+const Routes = () => {
+  return <Router history={hashHistory} routes={componentRoutes} />;
+};
+
+/*
+ * JSX structure routes
 const Routes = () => {
   return (
     <Router history={hashHistory}>
-      <Route path="/" component={Home}>
+      <Route path='/' component={Home}>
         <IndexRoute component={ArtistMain} />
-        <Route path="artists/new" component={ArtistCreate} />
-        <Route path="artists/:id" component={ArtistDetail} />
-        <Route path="artists/:id/edit" component={ArtistEdit} />
+        <Route path='artists/new' component={ArtistCreate} />
+        <Route path='artists/:id' component={ArtistDetail} />
+        <Route path='artists/:id/edit' component={ArtistEdit} />
       </Route>
     </Router>
   );
 };
+*/
 
 export default Routes;
